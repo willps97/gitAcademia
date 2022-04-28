ed8532eed7adf92310c07fe5c157987bec3c2feb
William Pereira dos Santos <william.sanctos@gmail.com>
2022-04-28 20:43:56 -0300

    exercicio 03

---
+++ Exercicio003/Exercicio003/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Escreva a seqüência de atribuições, em forma de programa em C# e no Visual Studio, para trocar entre 
+si os valores de duas variáveis A e B.*/
+
+Console.WriteLine("Digite o número da variável A: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número da variável B: ");
+double b = double.Parse(Console.ReadLine());
+
+double temp = 0.0;
+double temp2 = 0.0;
+
+temp = a;
+temp2 = b;
+b = temp;
+a = temp2;
+
+
+Console.WriteLine($"Os números A e B alterados são A = {a} e B = {b} ");
