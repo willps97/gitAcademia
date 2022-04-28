a6d2fd543a616d4431f7ede94830bb6e444d9a43
William Pereira dos Santos <william.sanctos@gmail.com>
2022-04-28 20:21:25 -0300

    Exercício 01

---
+++ Exercicio001/Exercicio001/Program.cs
@@ -0,0 +1,12 @@
+﻿/*1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética
+ * entre dois números quaisquer.*/
+
+Console.WriteLine("Digite um número: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro  número: ");
+double b = double.Parse(Console.ReadLine());
+
+double divisao = a / b;
+
+Console.WriteLine($"A divisão de {a} por {b} é de {divisao}");
