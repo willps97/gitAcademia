339daaeb74be597a78d5d8b8172072311c976c8d
William Pereira dos Santos <william.sanctos@gmail.com>
2022-05-05 14:51:04 -0300

    Exercicios propostos pelo professor em 04/05/2022.

---
+++ Segunda_Rodada_de_Exercicios/Exercicio001/Exercicio001/Program.cs
@@ -0,0 +1,23 @@
+﻿/*1 - Faça um algoritmo para calcular e mostrar a área de um triângulo a partir dos valores da base e da altura lidos pelo teclado: 
+ *Área do triangulo = (base * altura) / 2; 
+Teste se a base ou a altura digitada não foi igual a zero.*/
+
+
+Console.Write("Digite a base do triângulo:");
+double largura = double.Parse(Console.ReadLine());
+if (largura == 0)
+{
+    Console.WriteLine("a base não pode ser igual a zero.");
+}
+Console.Write("Digite a altura do triângulo:");
+double altura = double.Parse(Console.ReadLine());
+if (altura == 0)
+{
+    Console.WriteLine("a base não pode ser igual a zero.");
+}
+
+double triangulo = (largura * altura) / 2;
+
+Console.WriteLine("A área do triângulo é de: " + triangulo);
+
+
---
+++ Segunda_Rodada_de_Exercicios/Exercicio004/Exercicio004/Program.cs
@@ -0,0 +1,26 @@
+﻿/*4 - Escrever um algoritmo para ler a quantidade de horas aula dadas por dois professores e o valor por hora recebido por cada um deles.
+ *Mostrar na tela qual dos professores tem o maior salário total.
+ */
+
+Console.WriteLine("Digite a quantidade de horas/aula do professor A: ");
+int horasProfA = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a quantidade de horas/aula do professor B: ");
+int horasProfB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor (R$) da hora/aula do professor A: ");
+double valorHoraA = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor (R$) da hora/aula do professor B: ");
+double valorHoraB = double.Parse(Console.ReadLine());
+
+double totalProfessorA = horasProfA * valorHoraA;
+double totalProfessorB = horasProfB * valorHoraB;
+
+Console.WriteLine($"O salário do professor A é de: R$:{totalProfessorA}. O do professor B é de R$:{totalProfessorB}");
+if(totalProfessorA > totalProfessorB)
+{
+    Console.WriteLine("O salário do professor A é maior do que o do professor B.");
+}
+else
+{
+    Console.WriteLine("O salário do professor B é maior do que o do professor A.");
+}
