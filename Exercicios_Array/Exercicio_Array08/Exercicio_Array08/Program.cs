36a66f0ab6ab29eec5223681f5659cb7244c0a34
William Pereira dos Santos <william.sanctos@gmail.com>
2022-05-13 20:48:06 -0300

    Exercicio 08

---
+++ Exercicios_Array/Exercicio_Array08/Exercicio_Array08/Program.cs
@@ -0,0 +1,25 @@
+﻿/*8. Faça um programa em VS que solicite um numero inteiro. S
+e o numero não for inteiro, solicite-o até que seja. após, informe se ele é par ou impar. 
+Finalmente, pergunte ao usuário se ele quer repetir o programa em VS.*/
+
+
+int numero = -1;
+char validacao = 'S';
+
+while(validacao != 'N')
+{
+    Console.WriteLine("Digite um número inteiro: ");
+    numero = int.Parse(Console.ReadLine());
+    if(numero % 2 == 0)
+    {
+        Console.WriteLine("Numero par.");
+    }
+    else
+    {
+        Console.WriteLine("Numero Impart");
+    }
+
+    Console.WriteLine("Deseja repetir o programa? (S/N): ");
+    validacao = char.Parse(Console.ReadLine().ToUpper());
+}
+
